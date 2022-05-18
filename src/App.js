2d49251f2cd744af7614737e865782585c2f2166
Tuanbk <dodinhtuan681@gmail.com>
2022-05-18 17:31:32 +0700

    demo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import Navbar from "./component/Navbar";
+import Home from "./component/home";
+import { Routes, Route } from "react-router-dom";
+import Products from "./component/Products";
+import Product from "./component/Product";
+import Cart from "./component/Cart";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products" element={<Products />} />
+        <Route exact path="/products/:id" element={<Product />} />
+        <Route exact path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/redux/reducer/handleCart.js
@@ -0,0 +1,42 @@
+const cart = [];
+
+const handleCart = (state = cart, action) => {
+  const product = action.payload;
+
+  switch (action.type) {
+    case "ADDITEM":
+      //check if product is already Exist
+      const exist = state.find((item) => item.id === product.id);
+      if (exist) {
+        //increase the quantity
+        return state.map((item) =>
+          item.id === product.id ? { ...item, qty: item.qty + 1 } : item
+        );
+      } else {
+        const product = action.payload;
+        return [
+          ...state,
+          {
+            ...product,
+            pty: 1,
+          },
+        ];
+      }
+      break;
+
+    case "DELITEM":
+      const exist1 = state.find((item) => item.id === product.id);
+      if (exist1.qty === 1) {
+        return state.filter((item) => item.id === exist1.id);
+      } else {
+        return state.map((item) =>
+          item.id !== product.id ? { ...xdescribe, qty: item.qty } : item
+        );
+      }
+      break;
+    default:
+      return state;
+      break;
+  }
+};
+export default handleCart;
